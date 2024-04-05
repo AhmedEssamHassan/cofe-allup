3f66bdf5d83c63c512c83c1781ee4c9c893e4e52
AhmedEssamHassan <ahmedessamsakr22@gmail.com>
2024-04-05 06:32:43 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main_color: "#FC5007",
+        Dark_Charcoal: "#333333",
+        kernel: "#3F2016",
+        squant: "#666666",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
